e4a394a01956f96984a05c9af92e78b17f7c1dc8
Marcelo <marcelomab09@gmail.com>
2026-02-18 20:57:20 -0300

    fix: ajuste no env

---
+++ lib/core/config/supabase_config.dart
@@ -0,0 +1,4 @@
+class SupabaseConfig {
+  static const String url = 'https://pbdhxxixktekjzgfucwm.supabase.co';
+  static const String anonKey = 'sb_publishable_mHOOBibYEftti209CvA_dg_4NG4atPg';
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'dart:async';
-import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:mindease_focus/core/config/supabase_config.dart';
+
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
@@ -18,11 +19,11 @@ import 'package:mindease_focus/features/auth/presentation/controllers/focus_mode
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
-  await dotenv.load(fileName: ".env");
+
 
   await Supabase.initialize(
-    url: dotenv.env['SUPABASE_URL']!,
-    anonKey: dotenv.env['SUPABASE_ANON_KEY']!,
+    url: SupabaseConfig.url,
+    anonKey: SupabaseConfig.anonKey,
   );
 
   runApp(
